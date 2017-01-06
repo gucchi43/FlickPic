c09981ecf2ab45d2b89760059e11b0994691753e
Hiroki Taniguchi <acmikn43@gmail.com>
2017-01-06 17:55:39 +0900

    デザインを若者っぽく！

--- FlickPic/EffectlayerView.swift
+++ FlickPic/EffectlayerView.swift
@@ -13,21 +13,31 @@ import Colours
 class EffectlayerView: OverlayView {
 
     @IBOutlet weak var EffectlayerImageView: UIImageView!
+    @IBOutlet weak var rightActionLabel: UILabel!
 
+    @IBOutlet weak var leftActionLabel: UILabel!
 
     override var overlayState: SwipeResultDirection? {
         didSet {
             switch overlayState {
             case .left? :
                 let leftColor = UIColor.warning()
-                leftColor!.withAlphaComponent(0.5)
+                leftColor!.withAlphaComponent(0.3)
                 EffectlayerImageView.backgroundColor = leftColor
-                
+
+
+                leftActionLabel.isHidden = false
+                rightActionLabel.isHidden = true
+
 //                overlayImageView.image = UIImage(named: overlayLeftImageName)
             case .right? :
                 let rightColor = UIColor.success()
                 EffectlayerImageView.backgroundColor = rightColor
-                rightColor?.withAlphaComponent(0.5)
+                rightColor?.withAlphaComponent(0.3)
+
+                rightActionLabel.isHidden = false
+                leftActionLabel.isHidden = true
+
 //                overlayImageView.image = UIImage(named: overlayRightImageName)
             default:
                 EffectlayerImageView.image = nil
